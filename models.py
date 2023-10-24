c280e1b2e242305cb5daf9cb1180455c5a186510
Akanksha Pawar <42142815+akankshapawar1@users.noreply.github.com>
2023-10-23 23:28:48 -0400

    Hyperparameter tuning

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
-import xgboost as xgb
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, GridSearchCV
 from tqdm import tqdm
 from xgboost import XGBClassifier
 from xgboost import plot_importance
@@ -13,6 +12,8 @@ import warnings
 
 class XGBoostModel:
     # https://www.kaggle.com/code/gauthamyaramasa/cardiovascular-disease-prediction-xgboost/notebook
+    # commenting the code which uses train test splits. Doing 10-fold cross validation
+    """
     def __init__(self, x_train, y_train, x_test, y_test):
         self.x_train = x_train
         self.y_train = y_train
@@ -35,7 +36,7 @@ class XGBoostModel:
         plt.xlabel('Predicted')
         plt.ylabel('Actual')
         plt.title("Output Confusion Matrix")
-        # plt.show()
+        plt.show()
 
         print('True Positive Cases : {}'.format(cm[1][1]))
         print('True Negative Cases : {}'.format(cm[0][0]))
@@ -51,8 +52,8 @@ class XGBoostModel:
         print("The F1 Score is:", round(f1_score, 3))
         print("The Model Accuracy is:", round(accuracy_score(self.y_test, predict) * 100, 3), "%")
 
-        # plot_importance(model)
-        # plt.show()
+        plot_importance(model)
+        plt.show()
         # id is the most important feature according to the plot. Removed id from the dataframe.
 
         # Fit model using each importance as a threshold
@@ -72,12 +73,54 @@ class XGBoostModel:
                 predictions = [round(value) for value in y_pred]
                 accuracy = accuracy_score(self.y_test, predictions)
                 print("Thresh=%.3f, n=%d, Accuracy: %.2f%%" % (thresh, select_X_train.shape[1], accuracy * 100.0))
+        """
 
-    def cross_validate(self, k=5):
+    def __init__(self, x_data, y_data):
+        self.x_data = x_data
+        self.y_data = y_data
+
+    """def select_important_features(self, threshold=0.01):
+        # Train the model on the entire dataset
         model = XGBClassifier()
-        scores = cross_val_score(model, self.x_train, self.y_train, cv=k, scoring='accuracy', n_jobs=-1)
+        model.fit(self.x_data, self.y_data)
+
+        # Select features based on threshold
+        selection = SelectFromModel(model, threshold=threshold, prefit=True)
+        # plot_importance(model)
+        # plt.show()
+
+        # Printing feature importance scores
+        feature_importance = model.feature_importances_
+        sorted_idx = feature_importance.argsort()[::-1]  # Sort in descending order
+        for index in sorted_idx:
+            print(f"Feature: {self.x_data.columns[index]}, Importance: {feature_importance[index]}")
+        return selection.transform(self.x_data)
+
+    def cross_validate(self, k=10, feature_threshold=0.03):
+        model = XGBClassifier()
+        # to perform feature selection, we need to train the model on entire data.
+        reduced_x_data = self.select_important_features(threshold=feature_threshold)
+        scores = cross_val_score(model, reduced_x_data, self.y_data, cv=k, scoring='accuracy', n_jobs=-1)
 
         for i, score in enumerate(tqdm(scores, desc="Cross-validation", unit="fold")):
             print(f"Fold {i + 1} Accuracy: {score:.4f}")
 
-        print(f"Average Accuracy: {scores.mean():.4f} (+/- {scores.std() * 2:.4f})")
+        print(f"Average Accuracy: {scores.mean():.4f} (+/- {scores.std() * 2:.4f})")"""
+
+    def hyperparameter_tuning(self, k=10):
+        param_grid = {
+            'max_depth': [3, 4, 5],
+            'subsample': [0.6, 0.8, 1.0],
+            'learning_rate': [0.01, 0.02, 0.05, 0.1]
+        }
+
+        model = XGBClassifier()
+
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid,
+                                   scoring='accuracy', cv=k, verbose=1, n_jobs=-1)
+
+        grid_search.fit(self.x_data, self.y_data)
+
+        print(f"Best accuracy: {grid_search.best_score_} using {grid_search.best_params_}")
+
+        return grid_search.best_estimator_
